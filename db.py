a9abfc2b05558df91993074b848b729627729bcc
Maks <98954111+MunMaks@users.noreply.github.com>
2024-02-10 14:53:37 +0100

    Update db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import psycopg2.extras
 def connect():
   conn = psycopg2.connect(
     host = "sqletud.u-pem.fr",
-    dbname = "preno.nom_db",  # changer pour enter
+    dbname = "prenom.nom_db",  # changer pour enter
     password = "mot_de_pass", # changer pour enter
     cursor_factory = psycopg2.extras.NamedTupleCursor
   )
